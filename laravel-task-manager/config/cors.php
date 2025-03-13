4439339fc07b45b2c0772182be80dd25ffe6c3ac
Nafis-Iqbal <nafisiqbal10053@gmail.com>
2025-03-13 15:54:11 +0600

    #CORs commit 3

--- laravel-task-manager/config/cors.php
+++ laravel-task-manager/config/cors.php
@@ -12,14 +12,14 @@ return [
     | in web browsers. You are free to adjust these settings as needed.
     |
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
+    |'allowed_origins' => [env('CORS_ALLOW_URL_LOCAL'), env('CORS_ALLOW_URL_DEPLOY_1')],
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('CORS_ALLOW_URL_LOCAL'), env('CORS_ALLOW_URL_DEPLOY_1')],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
